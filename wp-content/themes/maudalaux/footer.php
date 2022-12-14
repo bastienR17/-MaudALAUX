8d129189ea2b57bbedb2d248db5a891e37a58a11
Angel Hmeli <angel.hmeli@edu.devinci.fr>
2022-12-14 22:24:48 +0100

    Accueil Responsive

--- wp-content/themes/maudalaux/footer.php
+++ wp-content/themes/maudalaux/footer.php
@@ -12,7 +12,7 @@
 ?>
 
 	<footer id="colophon" class="site-footer flex flex-col items-center">
-		<div class="site-info flex pt-12 m-auto">
+		<div class="site-info flex flex-col md:flex-row pt-12 m-auto items-center md:items-start gap-14 md:gap-0 mb-8 md:mb-0">
 			<?php
 				wp_nav_menu(
 					array(
@@ -20,7 +20,7 @@
 					)
 				);
 			?>
-			<div class="socials pl-[12vw] pr-[20vw] flex gap-7">
+			<div class="socials pl-[12vw] pr-[20vw] flex gap-7 ">
 				<a href="">
 					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/tiktok.png" alt="Logo TikTok">
 				</a>
